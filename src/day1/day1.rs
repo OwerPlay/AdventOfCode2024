84457bdf8114b7ffaba9ba53a892b5a0a3875ca8
Gašper Zajc <zajc.gasper@gmail.com>
2024-12-01 21:32:03 +0100

    Improvements to day one

--- src/day1/day1.rs
+++ src/day1/day1.rs
@@ -2,70 +2,51 @@ use crate::utils::utils;
 
 pub fn day1part1() -> String {
     let lines = utils::read_lines("input/day1.txt");
-    let mut left_input: Vec<String> = Vec::new();
-    let mut right_input: Vec<String> = Vec::new();
-
-    for line in lines {
-        let parts: Vec<String> = line.split("   ").map(|s| s.to_string()).collect();
-        left_input.push(parts[0].to_string());
-        right_input.push(parts[1].to_string());
-    }
+    let (mut left_input, mut right_input) = lines_to_u32(lines);
 
     let mut sum = 0;
     left_input.sort_unstable();
     right_input.sort_unstable();
 
-    for i in 0..left_input.len(){
-        if left_input[i] > right_input[i] {
-            sum += left_input[i].parse::<i32>().unwrap() - right_input[i].parse::<i32>().unwrap();
-        } else {
-            sum += right_input[i].parse::<i32>().unwrap() - left_input[i].parse::<i32>().unwrap();
-        }
-    }
+    left_input.iter().zip(&right_input).for_each(|(left, right)| {
+        sum += left.abs_diff(*right);
+    });
 
     sum.to_string()
 }
 
 pub fn day1part2() -> String {
     let lines = utils::read_lines("input/day1.txt");
-    let mut left_input: Vec<String> = Vec::new();
-    let mut right_input: Vec<String> = Vec::new();
-    for line in lines {
-        let parts: Vec<String> = line.split("   ").map(|s| s.to_string()).collect();
-        left_input.push(parts[0].to_string());
-        right_input.push(parts[1].to_string());
-    }
+    let (left_input, right_input) = lines_to_u32(lines);
+
     let mut sum = 0;
 
-    for left in left_input {
+    left_input.iter().for_each(|left| {
         let mut count = 0;
-        let mut left_number = 0;
-        for right in &right_input {
-            let left_parsed = left.parse::<i32>();
-            match left_parsed {
-                Ok(value) => {
-                    left_number = value;
-                }
-                Err(e) => {
-                    eprintln!("Failed to parse '{}': {}", left, e);
-                }
-            }
-            let right_parsed = right.parse::<i32>();
-            let mut right_number = 0;
-            match right_parsed {
-                Ok(value) => {
-                    right_number = value;
-                }
-                Err(e) => {
-                    eprintln!("Failed to parse '{}': {}", right, e);
-                }
-            }
-            if left_number == right_number {
+        right_input.iter().for_each(|right| {
+            if left == right {
                 count += 1;
             }
-        }
-        sum += count * left_number;
-    }
+        });
+        sum += count * left;
+    });
 
     sum.to_string()
+}
+
+fn lines_to_u32(lines: Vec<String>) -> (Vec<u32>, Vec<u32>) {
+    let mut left_input: Vec<u32> = Vec::new();
+    let mut right_input: Vec<u32> = Vec::new();
+
+    lines.iter().for_each(|line| {
+        let parts: Vec<Result<u32, _>> = line.split_whitespace().map(|s| s.parse()).collect();
+        if let (Some(Ok(left)), Some(Ok(right))) = (parts.get(0), parts.get(1)) {
+            left_input.push(*left);
+            right_input.push(*right);
+        } else {
+            eprintln!("Failed to parse line: {}", line);
+        }
+    });
+
+    (left_input, right_input)
 }
